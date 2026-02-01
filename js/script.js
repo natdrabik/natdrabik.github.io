e30808f5ebf66cbc0537e827079920d9e454d556
Nathalie Almeida <45579354+natdrabik@users.noreply.github.com>
2026-02-01 15:16:05 -0300

    nav mobile

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 var btMenu = document.getElementById('btMenu');
 var menu = document.getElementById('menu');
+var linksMenu = menu.querySelectorAll('a');
 
 btMenu.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
     document.body.classList.toggle('has-menu');
+}
+
+for (linkMenu of linksMenu) {
+    linkMenu.addEventListener('click', closeMenu);
+}
+
+function closeMenu() {
+    document.body.classList.remove('has-menu');
 }
